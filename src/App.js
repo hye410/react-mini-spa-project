a2e5e7d6d46fc2661a65eee7cbda615f0718a0e4
hye410 <dhye410@daum.net>
2023-06-26 16:51:28 +0900

    자잘한 오류 수정 및 기본 비밀번호 핸드폰 뒷자리로 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import { useState } from 'react';
 
 function App({children}) {
 
